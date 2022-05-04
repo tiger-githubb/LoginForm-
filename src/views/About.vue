481fb135ae5560c1e71d8c346bc3bf9e4441368a
tiger-githubb <aristidetiger12@gmail.com>
2022-05-04 23:59:24 +0000

    work on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,17 @@
   <div class="about">
     <h1>cette page est en cours de construction</h1>
     <h3>Ici est une page à propos</h3>
+
+    <lottie-player
+      src="https://assets7.lottiefiles.com/packages/lf20_ndkusx2f.json"
+      background="transparent"
+      speed="1"
+      style="width: 300px; height: 300px"
+      loop
+      controls
+      autoplay
+    ></lottie-player>
   </div>
 </template>
+
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
